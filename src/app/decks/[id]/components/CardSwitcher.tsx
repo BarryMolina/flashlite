760ea25001a966f78787cbb1d4b518a501c61a5e
Barry Molina <bazzaboy@gmail.com>
2024-06-09 00:11:49 -0500

    implement flip card button

--- src/app/decks/[id]/components/CardSwitcher.tsx
+++ src/app/decks/[id]/components/CardSwitcher.tsx
@@ -11,8 +11,20 @@ import NextIcon from "@/app/assets/icons/next.svg";
 
 export function CardSwitcher({ cards }: { cards: Card[] }) {
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [flip, setFlip] = useState(false);
-  const [shuffle, setShuffle] = useState(false);
+  const [flipped, setFlipped] = useState(false);
+  const [shuffled, setShuffled] = useState(false);
+  const [flippedCards, setFlippedCards] = useState(
+    Array<boolean>(cards.length).fill(false)
+  );
+
+  function flipCards() {
+    setFlipped(!flipped);
+    setFlippedCards(Array(flippedCards.length).fill(!flipped));
+  }
+
+  function shuffleCards() {
+    setShuffled(!shuffled);
+  }
 
   function prevCard() {
     setSelectedIndex(selectedIndex > 0 ? selectedIndex - 1 : cards.length - 1);
@@ -22,17 +34,23 @@ export function CardSwitcher({ cards }: { cards: Card[] }) {
     setSelectedIndex(selectedIndex < cards.length - 1 ? selectedIndex + 1 : 0);
   }
 
+  function toggleFlipped(index: number) {
+    const newCards = [...flippedCards];
+    newCards[index] = !flippedCards[index];
+    setFlippedCards(newCards);
+  }
+
   return (
     <div className="flex flex-col gap-4 items-center">
       <div className="flex justify-between w-[500px]">
-        <IconButton active={flip} onClick={() => setFlip(!flip)}>
+        <IconButton active={flipped} onClick={flipCards}>
           <FlipIcon />
         </IconButton>
         <CardPagination
           currentPage={selectedIndex + 1}
           totalPages={cards.length}
         />
-        <IconButton active={shuffle} onClick={() => setShuffle(!shuffle)}>
+        <IconButton active={shuffled} onClick={shuffleCards}>
           <ShuffleIcon />
         </IconButton>
       </div>
@@ -41,6 +59,8 @@ export function CardSwitcher({ cards }: { cards: Card[] }) {
           <PrevIcon />
         </IconButton>
         <FlashCard
+          flipped={flippedCards[selectedIndex]}
+          toggleFlipped={() => toggleFlipped(selectedIndex)}
           key={cards[selectedIndex].definition}
           card={cards[selectedIndex]}
         />
--- src/app/decks/[id]/components/FlashCard.tsx
+++ src/app/decks/[id]/components/FlashCard.tsx
@@ -2,13 +2,16 @@
 import { Card } from "@/app/data/types";
 import { useState } from "react";
 
-export function FlashCard({ card }: { card: Card }) {
-  const [flipped, setFlipped] = useState(false);
+export function FlashCard(props: {
+  card: Card;
+  flipped: boolean;
+  toggleFlipped: () => void;
+}) {
   return (
-    <button onClick={() => setFlipped(!flipped)}>
+    <button onClick={props.toggleFlipped}>
       <div className="w-[500px] min-h-[300px] p-10 border flex items-center justify-center drop-shadow-md bg-white">
         <h3 className="text-lg">
-          {flipped ? card.definition : card.description}
+          {props.flipped ? props.card.definition : props.card.description}
         </h3>
       </div>
     </button>
